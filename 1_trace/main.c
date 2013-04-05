2ff107fb44445d883c5e3cbd842c74a19162df01
Jason Dreisbach <jtdreisb@gmail.com>
2013-04-05 13:01:38 -0700

    pcap

--- 1_trace/main.c
+++ 1_trace/main.c
@@ -3,8 +3,38 @@
 
 #include "trace.h"
 #include <stdio.h>
+#include <pcap/pcap.h>
+
+void usage()
+{
+	printf("usage:\n\ttrace <pcap filename> \n");
+}
+
 int main(int argc, char const *argv[])
 {
-	printf("hello\n");
+	char *inputFileName = NULL;
+	char errbuf[PCAP_ERRBUF_SIZE];
+	pcap_t *p = NULL;
+	struct pcap_pkthdr *pkt_header;
+	u_char *pkt_data = NULL;
+	int pcap_status = 1;
+
+	if (argc == 2) {
+		inputFileName = argv[1];
+	}
+	else {
+		usage();
+		exit(1);
+	}
+
+
+	p = pcap_open_offline(inputFileName, errbuf);
+	while (pcap_status == 1) {
+		pcap_status =  pcap_next_ex(p, &pkt_header, &pkt_data);
+		// begin decoding the file
+		printf("readpacket\n");
+	}
+	pcap_close(p);
+	
 	return 0;
 }
